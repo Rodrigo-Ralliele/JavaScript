d9a0891c690a6f86c69c6c58845f101c7545642b
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-11-13 23:15:16 -0300

    Create node_907525a903b7c.tmp.js

---
+++ aula13/node_907525a903b7c.tmp.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log ('Tudo certo')
+} while ( c <=6)
+
+
+/*var c =  1
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++
+}*/
+
+
+/*console.log ("Tudo Bem!")
+console.log ("Tudo Bem!")
+console.log ("Tudo Bem!")
+console.log ("Tudo Bem!")
+console.log ("Tudo Bem!")
+console.log ("Tudo Bem!")*/

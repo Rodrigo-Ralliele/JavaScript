08ad6718793bece2d5d60572f5c9bcdf562a35ea
Rodrigo Ralliele <146042299+Rodrigo-Ralliele@users.noreply.github.com>
2023-11-14 22:59:32 -0300

    daw

--- aula12EX/ex14/script.js
+++ aula12EX/ex14/script.js
@@ -1,1 +1,21 @@
-alert ('Ola!')
+function carregar() {
+    var msg = document.getElementById ('msg')
+    var img = document.getElementById ('imagem')
+    var data = new Date ()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >=0 && hora <12) {
+        //BOM DIA!!
+        document.body.style.background = '#D9B752'
+        img.src = 'fotomanha.png'
+    } else if ( hora >=12 && hora <18) {
+        //BOA TARDE
+        document.body.style.background = '#F28749'
+        img.src ='fototarde.png'
+    } else {
+        document.body.style.background = '#000D0C'
+        //BOA NOITE
+        img.src ='fotonoite.png'
+    }
+}
+
